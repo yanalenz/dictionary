5d37a7ab281008bc3e6698af3cc107282a6ab3b1
yana <yanache51@gmail.com>
2026-02-09 16:40:02 +0300

    Improved adding dates to the dictionary

--- dictionary.py
+++ dictionary.py
@@ -4,6 +4,7 @@ import os
 from dataclasses import dataclass, asdict 
 from nicegui import ui
 from deep_translator import GoogleTranslator
+from datetime import datetime
 
 FILE_PATH = 'dictionary.json'
 
@@ -12,6 +13,7 @@ FILE_PATH = 'dictionary.json'
 class WordPair:
     english: str
     russian: str
+    date: str
 
 DICTIONARY_DB = {'cat': 'кот', 'dog': 'собака'}
 
@@ -37,8 +39,13 @@ def main_page():
 
     @ui.refreshable
     def render_list():
+        last_date = None
         with ui.column().classes('w-full gap-2 mt-4 bg-zinc-700'):
-            for item in words_list:
+            for item in reversed(words_list):
+                item_date = item.date.split()[0]
+                if item_date != last_date:
+                    ui.label(item_date).classes('text-xs text-zinc-400 mt-3 mb-1 font-semibold')
+                last_date = item_date
                 with ui.row().classes('w-full items-center justify-between bg-zinc-700 rounded p-2'):
                     with ui.row().classes('gap-4'):
                         ui.label(item.english).classes('w-16 text-sm font-bold text-bg-red-200')
@@ -46,12 +53,15 @@ def main_page():
                         ui.label(item.russian).classes('italic text-sm text-bg-red-100')
                     ui.button(icon='delete', on_click=lambda i=item: remove_word(i)) \
                         .props('flat fab-mini color=bg-stone-300')
+                    
 
     async def add_word():
         text = input_field.value.strip().lower()
+        current_time = datetime.now().strftime('%d.%m.%Y %H:%M')
         if not text:
             return
         
+
         # search through the API
         if text in DICTIONARY_DB:
             translation = DICTIONARY_DB[text]
@@ -63,7 +73,7 @@ def main_page():
             ui.notify(f'Ошибка: {e}', color='red')
 
         # Добавляем, Сохраняем и Обновляем
-        words_list.append(WordPair(text, translation))
+        words_list.append(WordPair(text, translation, current_time))
         save_data(words_list)
         input_field.value = ''
         render_list.refresh()
@@ -81,6 +91,7 @@ def main_page():
         ui.button('Add', on_click=add_word).props('color=bg-red-950').classes('w-full mt-2')
         ui.separator().classes('my-4')
         render_list()
+
 
 
 ui.run(native=True, dark=True, title="Dictionary App")
